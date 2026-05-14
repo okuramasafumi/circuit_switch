4236a66f28bfa2cb421af79da28b01be147f13a5
OKURA Masafumi <masafumi.o1988@gmail.com>
2026-05-14 23:58:54 +0900

    Remove default value from YARD comment
    It should automatically generate default value doc.

--- lib/circuit_switch.rb
+++ lib/circuit_switch.rb
@@ -17,12 +17,12 @@ module CircuitSwitch
     end
 
     # @param key [String] Named key to find switch instead of caller
-    # @param if [Boolean, Proc] Call proc when `if` is truthy (default: true)
-    # @param close_if [Boolean, Proc] Call proc when `close_if` is falsy (default: false)
-    # @param close_if_reach_limit [Boolean] Stop calling proc when run count reaches limit (default: false)
+    # @param if [Boolean, Proc] Call proc when `if` is truthy
+    # @param close_if [Boolean, Proc] Call proc when `close_if` is falsy
+    # @param close_if_reach_limit [Boolean] Stop calling proc when run count reaches limit
     # @param limit_count [Integer] Limit count. Use `run_limit_count` default value if it's nil
-    #   Can't be set 0 when `close_if_reach_limit` is true (default: nil)
-    # @param initially_closed [Boolean] Create switch with terminated mode (default: false)
+    #   Can't be set 0 when `close_if_reach_limit` is true
+    # @param initially_closed [Boolean] Create switch with terminated mode
     # @param [Proc] block
     def run(key: nil, if: nil, close_if: nil, close_if_reach_limit: nil, limit_count: nil, initially_closed: nil, &block)
       arguments = {
@@ -37,11 +37,11 @@ module CircuitSwitch
     end
 
     # @param key [String] Named key to find switch instead of caller
-    # @param if [Boolean, Proc] Report when `if` is truthy (default: true)
-    # @param stop_report_if [Boolean, Proc] Report when `close_if` is falsy (default: false)
-    # @param stop_report_if_reach_limit [Boolean] Stop reporting when reported count reaches limit (default: true)
+    # @param if [Boolean, Proc] Report when `if` is truthy
+    # @param stop_report_if [Boolean, Proc] Report when `close_if` is falsy
+    # @param stop_report_if_reach_limit [Boolean] Stop reporting when reported count reaches limit
     # @param limit_count [Integer] Limit count. Use `report_limit_count` default value if it's nil
-    #   Can't be set 0 when `stop_report_if_reach_limit` is true (default: nil)
+    #   Can't be set 0 when `stop_report_if_reach_limit` is true
     def report(key: nil, if: nil, stop_report_if: nil, stop_report_if_reach_limit: nil, limit_count: nil)
       if block_given?
         raise ArgumentError.new('CircuitSwitch.report doesn\'t receive block. Use CircuitSwitch.run if you want to pass block.')
@@ -59,11 +59,11 @@ module CircuitSwitch
 
     # Syntax sugar for `CircuitSwitch.run`
     # @param key [String] Named key to find switch instead of caller
-    # @param if [Boolean, Proc] `CircuitSwitch.run` is runnable when `if` is truthy (default: true)
-    # @param close_if [Boolean, Proc] `CircuitSwitch.run` is runnable when `close_if` is falsy (default: false)
-    # @param close_if_reach_limit [Boolean] `CircuitSwitch.run` is NOT runnable when run count reaches limit (default: true)
-    # @param limit_count [Integer] Limit count. Use `run_limit_count` default value if it's nil. Can't be set 0 (default: nil)
-    # @param initially_closed [Boolean] Create switch with terminated mode (default: false)
+    # @param if [Boolean, Proc] `CircuitSwitch.run` is runnable when `if` is truthy
+    # @param close_if [Boolean, Proc] `CircuitSwitch.run` is runnable when `close_if` is falsy
+    # @param close_if_reach_limit [Boolean] `CircuitSwitch.run` is NOT runnable when run count reaches limit
+    # @param limit_count [Integer] Limit count. Use `run_limit_count` default value if it's nil. Can't be set 0
+    # @param initially_closed [Boolean] Create switch with terminated mode
     # @return [Boolean]
     def open?(key: nil, if: nil, close_if: nil, close_if_reach_limit: nil, limit_count: nil, initially_closed: nil)
       if block_given?
